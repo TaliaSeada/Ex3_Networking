8b4139c94bd6d9c8dc6c932e61efa9408fc68e63
LiorBreitman8234 <liorbreitman321@gmail.com>
2022-05-06 15:18:04 +0300

    updated client

--- client.py
+++ client.py
@@ -4,13 +4,6 @@ from socket import *
 # for command line input
 import sys
 
-# to check if socket is up
-def is_connect(sock):
-	try:
-		sock.sendall(b"ping")
-		return True
-	except:
-		return False
 	
 while True:
 	try:
@@ -30,18 +23,16 @@ while True:
         # opening file
 		f = open(file, "w")
 		while True:
-			if is_connect(clientSocket) == False:
-				break
             # getting the file content
 			answer = clientSocket.recv(1024)
 			f.write(answer.decode())
-		break
 	except IOError as e:
 		error = str(e)
 		split1 = error.split()[1]
 		split2 = split1.split(']')[0]
 		if int(split2) != 104:
 			print("error!!! " + str(e))
+			break
 
 print("finished getting file")
 clientSocket.close()
